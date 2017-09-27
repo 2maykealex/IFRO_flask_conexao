0e341bb12d100165a1806b7547b1540092b9b441
Mayke <mayke.suporte@gmail.com>
2017-09-26 21:38:02 -0400

    Adicionando uma lista

--- Mayke.py
+++ Mayke.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+Lista = [1,2,3]
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        Lista.append(request.form['texto'])
+    return render_template('index.html', items=Lista)
 
 
 if __name__ == '__main__':
